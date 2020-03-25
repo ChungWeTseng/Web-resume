f361a62f64002273ea42c3a11fdcfbbb84ae7fee
Tsewe <chungwe1994@gmail.com>
2020-03-25 18:47:50 +0100

    Added navbar

--- src/components/header.js
+++ src/components/header.js
@@ -1,33 +1,30 @@
-import { Link } from "gatsby"
-import PropTypes from "prop-types"
-import React from "react"
+import PropTypes from 'prop-types'
+import React from 'react'
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
+  <header>
+    <nav class='navbar is-dark' role='navigation' aria-label='main-navigation'>
+      <div class='navbar-brand'>
+        <a class='navbar-item' href='#'>
+          <img></img>
+          <h1 class='has-text-weight-bold'> Chung We Tseng Fang </h1>
+        </a>
+        <a role='button' class='navbar-burger burger' aria-label='menu' aria-expanded='false' data-target='navbarBasicExample'>
+          <span aria-hidden='true'></span>
+          <span aria-hidden='true'></span>
+          <span aria-hidden='true'></span>
+        </a>
+      </div>
+      <div class='navbar-menu'>
+        <div class='navbar-end'>
+          <a class='navbar-item' href='#'> Experience </a>
+          <a class='navbar-item' href='#'> Education </a>
+          <a class='navbar-item' href='#'> Skills & Languages </a>
+          <a class='navbar-item' href='#'> Portfolio </a>
+          <a class='navbar-item' href='#'> Contact </a>
+        </div>
+      </div>
+    </nav>
   </header>
 )
 
