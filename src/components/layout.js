792fc53c16bdce2555b298613eb16ddcdcd2a4b6
Tsewe <chungwe1994@gmail.com>
2020-04-26 20:46:32 +0200

    Added footer

--- src/components/layout.js
+++ src/components/layout.js
@@ -27,10 +27,10 @@ const Layout = ({ children }) => {
     <>
       <Header siteTitle={data.site.siteMetadata.title} />
       <main>{children}</main>
-      <footer>
-        © {new Date().getFullYear()}, Built with
-        {` `}
-        <a href="https://www.gatsbyjs.org">Gatsby</a>
+      <footer className='footer'>
+        <div className='content has-text-centered'>
+          © Copyright 2020 Chung We Tseng Fang
+        </div>
       </footer>
     </>
   )
